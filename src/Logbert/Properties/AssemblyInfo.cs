01f6e9f8ae23068fb712ecf8c53e6ecd824a7e4b
couchcoding <github@couchcoding.com>
2017-05-03 14:04:24 +0200

    Increased version

--- src/Logbert/Properties/AssemblyInfo.cs
+++ src/Logbert/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.1.1")]
-[assembly: AssemblyFileVersion("1.3.1.1")]
+[assembly: AssemblyVersion("1.3.1.2")]
+[assembly: AssemblyFileVersion("1.3.1.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
