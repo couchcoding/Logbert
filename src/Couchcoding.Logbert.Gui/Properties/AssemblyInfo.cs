691e8bc61031d9c219e07fbfafad9156f741fa24
couchcoding <github@couchcoding.com>
2020-03-31 10:57:36 +0200

    Updated copyright year and increased application version

--- src/Couchcoding.Logbert.Gui/Properties/AssemblyInfo.cs
+++ src/Couchcoding.Logbert.Gui/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Couchcoding")]
 [assembly: AssemblyProduct("Logbert Control Library")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Couchcoding")]
+[assembly: AssemblyCopyright("Copyright © 2020 Couchcoding")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.1")]
-[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
