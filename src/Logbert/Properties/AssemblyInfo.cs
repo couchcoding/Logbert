3506582ab1b97612c2213af485c6591995f21513
Michael Wangler <michael.wangler@aleon.eu>
2021-03-18 07:41:53 +0100

    Updated assembly version

--- src/Logbert/Properties/AssemblyInfo.cs
+++ src/Logbert/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.3.0")]
-[assembly: AssemblyFileVersion("1.6.3.0")]
+[assembly: AssemblyVersion("1.6.3.1")]
+[assembly: AssemblyFileVersion("1.6.3.1")]
 [assembly: NeutralResourcesLanguageAttribute("")]
